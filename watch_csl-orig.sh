6e2c45324d23bbd651a3a9779e7b7147a8db45b5
Francisco Mondaca <f.mondaca@uni-koeln.de>
2020-03-24 21:44:53 +0100

    indentation up

--- watch_csl-orig.sh
+++ watch_csl-orig.sh
@@ -15,47 +15,41 @@ inotifywait -m -r ${1} -e close_write|
     while read path action file; do
         dict_id=`basename "$path"`
         echo "$dict_id has been modified"
-		cd ${2}
+		    cd ${2}
+        echo "current dir $PWD"
 
-		echo "current dir $PWD" 
+        # generate_orig.py
+        python generate.py "${dict_id}" inventory_orig.txt _ "${1}${dict_id}" ${3}${dict_id}
 
-		# generate_orig.py	
-		python generate.py "${dict_id}" inventory_orig.txt _ "${1}${dict_id}" ${3}${dict_id}
+        echo "${3}${dict_id} has been generated"
 
-		echo "${3}${dict_id} has been generated" 
+        # generate_pywork.py
+        python generate.py "${dict_id}" inventory.txt  makotemplates distinctfiles/${dict_id} ${3}${dict_id}
 
-		# generate_pywork.py
-		python generate.py "${dict_id}" inventory.txt  makotemplates distinctfiles/${dict_id} ${3}${dict_id}
-		
-		# go to the generated local instance
-		cd ${3}${dict_id}/pywork
+        # go to the generated local instance
+        cd ${3}${dict_id}/pywork
 
-		echo "regenerate ${dict_id} headwords"
-		sh redo_hw.sh
+        echo "regenerate ${dict_id} headwords"
+        sh redo_hw.sh
 
-		echo "regenerate ${dict_id}.xml files"
-		sh redo_xml.sh
+        echo "regenerate ${dict_id}.xml files"
+        sh redo_xml.sh
 
-		# mkdir if not available
-	  	mkdir -p ${4}${dict_id}
+        # mkdir if not available
+        mkdir -p ${4}${dict_id}
 
-	  	# copy xml file fo csl-kosh_generated
-    	cp  ${dict_id}.xml ${4}${dict_id}/${dict_id}.xml
-    
-    	d_hour=$(date +"%T")
-    	d_date=$(date +"%F")
+        # copy xml file fo csl-kosh_generated
+        cp  ${dict_id}.xml ${4}${dict_id}/${dict_id}.xml
 
+        d_hour=$(date +"%T")
+        d_date=$(date +"%F")
 
-    	echo "${d_date} ${d_hour} ${dict_id}.xml copied to ${4}${dict_id}"
+    	  echo "${d_date} ${d_hour} ${dict_id}.xml copied to ${4}${dict_id}"
+    	  log_file=$6
 
+        if [ ! -f "$6" ]; then
+          mkdir -p "`dirname \"$6\"`" 2>/dev/null
+        fi
 
-    	log_file=$6
-
-    	  if [ ! -f "$6" ]; then
-    	      mkdir -p "`dirname \"$6\"`" 2>/dev/null
-    	  fi
-
-    	echo "${d_date} ${d_hour} ${dict_id}.xml" >> ${log_file}
-
-
+    	  echo "${d_date} ${d_hour} ${dict_id}.xml" >> ${log_file}
     done
